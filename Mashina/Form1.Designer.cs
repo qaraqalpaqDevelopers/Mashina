21f6f447157fe83161f795bdd714e2998f6a06aa
USER <USER@WIN-QGII8ID360C>
2022-08-28 12:00:59 +0500

    Add project files.

---
+++ Mashina/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Mashina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.a1 = new System.Windows.Forms.Label();
+            this.waqit = new System.Windows.Forms.Timer(this.components);
+            this.a3 = new System.Windows.Forms.Label();
+            this.a4 = new System.Windows.Forms.Label();
+            this.a2 = new System.Windows.Forms.Label();
+            this.a7 = new System.Windows.Forms.Label();
+            this.a6 = new System.Windows.Forms.Label();
+            this.a5 = new System.Windows.Forms.Label();
+            this.a8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.SystemColors.Info;
+            this.a1.Location = new System.Drawing.Point(351, 24);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(10, 67);
+            this.a1.TabIndex = 9;
+            // 
+            // waqit
+            // 
+            this.waqit.Interval = 10;
+            this.waqit.Tick += new System.EventHandler(this.waqit_Tick);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.SystemColors.Info;
+            this.a3.Location = new System.Drawing.Point(351, 315);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(10, 67);
+            this.a3.TabIndex = 29;
+            // 
+            // a4
+            // 
+            this.a4.BackColor = System.Drawing.SystemColors.Info;
+            this.a4.Location = new System.Drawing.Point(351, 468);
+            this.a4.Name = "a4";
+            this.a4.Size = new System.Drawing.Size(10, 67);
+            this.a4.TabIndex = 30;
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.SystemColors.Info;
+            this.a2.Location = new System.Drawing.Point(351, 171);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(10, 67);
+            this.a2.TabIndex = 31;
+            // 
+            // a7
+            // 
+            this.a7.BackColor = System.Drawing.SystemColors.Info;
+            this.a7.Location = new System.Drawing.Point(503, 315);
+            this.a7.Name = "a7";
+            this.a7.Size = new System.Drawing.Size(10, 67);
+            this.a7.TabIndex = 32;
+            // 
+            // a6
+            // 
+            this.a6.BackColor = System.Drawing.SystemColors.Info;
+            this.a6.Location = new System.Drawing.Point(503, 171);
+            this.a6.Name = "a6";
+            this.a6.Size = new System.Drawing.Size(10, 67);
+            this.a6.TabIndex = 33;
+            // 
+            // a5
+            // 
+            this.a5.BackColor = System.Drawing.SystemColors.Info;
+            this.a5.Location = new System.Drawing.Point(503, 24);
+            this.a5.Name = "a5";
+            this.a5.Size = new System.Drawing.Size(10, 67);
+            this.a5.TabIndex = 34;
+            // 
+            // a8
+            // 
+            this.a8.BackColor = System.Drawing.SystemColors.Info;
+            this.a8.Location = new System.Drawing.Point(503, 468);
+            this.a8.Name = "a8";
+            this.a8.Size = new System.Drawing.Size(10, 67);
+            this.a8.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(223, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(9, 587);
+            this.label2.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(643, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(9, 587);
+            this.label3.TabIndex = 37;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 493);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 53);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Baslaw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gold;
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(6, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(211, 587);
+            this.panel2.TabIndex = 40;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Mashina.Properties.Resources.palm31;
+            this.pictureBox4.Location = new System.Drawing.Point(26, 78);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(131, 126);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 25;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Mashina.Properties.Resources.palma;
+            this.pictureBox2.Location = new System.Drawing.Point(26, 363);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Mashina.Properties.Resources.palma;
+            this.pictureBox3.Location = new System.Drawing.Point(38, 48);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(131, 126);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 24;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Mashina.Properties.Resources.palm31;
+            this.pictureBox5.Location = new System.Drawing.Point(38, 314);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(131, 126);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 26;
+            this.pictureBox5.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Location = new System.Drawing.Point(658, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(218, 587);
+            this.panel1.TabIndex = 39;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox7.Image = global::Mashina.Properties.Resources.car1;
+            this.pictureBox7.Location = new System.Drawing.Point(381, 295);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(94, 111);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 42;
+            this.pictureBox7.TabStop = false;
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.Color.DimGray;
+            this.car.Image = global::Mashina.Properties.Resources.c_2;
+            this.car.Location = new System.Drawing.Point(238, 468);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(94, 111);
+            this.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car.TabIndex = 38;
+            this.car.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox6.Image = global::Mashina.Properties.Resources.car1;
+            this.pictureBox6.Location = new System.Drawing.Point(531, 9);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(94, 111);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 27;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(874, 591);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.a8);
+            this.Controls.Add(this.a5);
+            this.Controls.Add(this.a6);
+            this.Controls.Add(this.a7);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a4);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.a1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer waqit;
+        private System.Windows.Forms.Label a3;
+        private System.Windows.Forms.Label a4;
+        private System.Windows.Forms.Label a2;
+        private System.Windows.Forms.Label a7;
+        private System.Windows.Forms.Label a6;
+        private System.Windows.Forms.Label a5;
+        private System.Windows.Forms.Label a8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox car;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
+
---
+++ Mashina/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using WMPLib;
+
+namespace Mashina
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player=new WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void jaylastiriw()
+        {
+            y2 = pictureBox2.Location.Y;
+            y3 = pictureBox3.Location.Y;
+            y4 = pictureBox4.Location.Y;
+            y5 = pictureBox5.Location.Y;
+            x6= pictureBox6.Location.X;
+            x7= pictureBox7.Location.X;
+
+            y6 = pictureBox6.Location.Y;
+            y7 = pictureBox7.Location.Y;
+           
+            
+            b1 = a1.Location.Y;
+            b2= a2.Location.Y;
+            b3= a3.Location.Y;
+            b4= a4.Location.Y;
+            b5= a5.Location.Y;
+            b6 = a6.Location.Y;
+            b7 = a7.Location.Y;
+            b8 = a8.Location.Y;
+            mx = car.Location.X;
+            my = car.Location.Y;
+           
+
+        }
+      
+        
+
+        
+
+        
+        int sekund = 0,y2=0, y3 = 0, y4 = 0, y5 = 0,x6=0,x7=0, y6 = 0,y7=0,mx=0,my=0,tik=0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tik++;
+        }
+
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left&&car.Location.X>190)
+            {
+                mx -= 25;
+                car.Location = new Point(mx,my);
+               
+            }
+            else if(e.KeyCode == Keys.Right&&car.Location.X < 400)
+            {
+                mx += 25;
+                car.Location=new Point(mx,my);
+            }
+
+        }
+       
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            player.URL = @"ferrari.Mp3";
+            jaylastiriw();
+            timer1.Start();
+            waqit.Start();
+            button1.Visible = false;
+            
+        }
+
+        int b1 =0,b2=0,b3=0, b4=0, b5=0, b6=0, b7=0, b8=0;
+
+        private void waqit_Tick(object sender, EventArgs e)
+        {
+            if(car.Bounds.IntersectsWith(pictureBox6.Bounds))  
+            {
+                timer1.Stop();
+                waqit.Stop();
+            }
+            if (car.Bounds.IntersectsWith(pictureBox7.Bounds))
+            {
+                timer1.Stop();
+                waqit.Stop();
+            }
+
+            sekund++;
+           
+            y2 +=5;
+            pictureBox2.Location=new Point(pictureBox2.Location.X,y2);
+            y3+=5;
+            pictureBox3.Location = new Point(pictureBox3.Location.X, y3);
+            y4+=5;
+            pictureBox4.Location = new Point(pictureBox4.Location.X, y4);
+            y5+=5;
+            pictureBox5.Location = new Point(pictureBox5.Location.X, y5);
+            y6+=4;
+            pictureBox6.Location = new Point(x6, y6);
+            y7+=4;
+            pictureBox7.Location = new Point(x7, y7);
+
+
+            b1 += 8;
+            a1.Location = new Point(a1.Location.X, b1);
+            b2 += 8;
+            a2.Location = new Point(a2.Location.X, b2);
+            b3 += 8;
+            a3.Location = new Point(a3.Location.X, b3);
+            b4 += 8;
+            a4.Location = new Point(a4.Location.X, b4);
+            b5 += 8;
+            a5.Location = new Point(a5.Location.X, b5);
+            b6 += 8;
+            a6.Location = new Point(a6.Location.X, b6);
+            b7 += 8;
+            a7.Location = new Point(a7.Location.X, b7);
+            b8 += 8;
+            a8.Location = new Point(a8.Location.X, b8);
+
+            if (y7 > 500)
+            {
+
+
+                if (tik % 3 == 0)
+                  x7 = 290;
+                else if (tik % 3 == 1)
+                    x7 = 400;
+                else if (tik % 3 == 2)
+                    x7 = 180;
+                y7 = -100;
+            }
+
+            if (y2 > 500)
+                y2 = 0;
+            if (y3 > 500)
+                y3 =0;
+            if (y4 > 500)
+                y4 = 0;
+            if (y5 > 500)
+                y5 = 0;
+           
+            if (y6 > 500)
+            {
+               
+
+                if (tik % 3 == 0) 
+                    x6 = 180;
+                else if (tik % 3 == 1)
+                    x6 = 290;
+                else if (tik % 3 == 2)
+                    x6 = 400;
+                y6 = -100;
+            }
+           
+
+
+
+            if (b1 > 500)
+                b1 = 0;
+            if (b2 > 500)
+                b2 = 0;
+            if (b3 > 500)
+                b3 = 0;
+            if (b4 > 500)
+                b4 = 0;
+            if (b5 > 500)
+                b5 = 0;
+            if (b6 > 500)
+                b6 = 0;
+            if (b7 > 500)
+                b7 = 0;
+            if (b8 > 500)
+                b8 = 0;
+
+        }
+    }
+}
